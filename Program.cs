7679d2796b17e442c737e7d275e63bc9f694097e
Dmitry2707 <119356448+Dmitry2707@users.noreply.github.com>
2022-12-06 02:16:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("write numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write numberC");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < numberB && numberB < numberC)
+{
+    Console.WriteLine("numberC = max"); 
+   
+
+}
